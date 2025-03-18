291ea16cbfbb594009c1d2f2b73793f0d94f8a9e
darshan45672 <drshnbhandary@gmail.com>
2025-03-18 08:50:40 +0530

    day 1

---
+++ day1/assignment.java
@@ -0,0 +1,14 @@
+// Nitish is a IT student at ANITS.
+// He is very much interested in programming.
+// He has 3 Internals in his 6th semester
+// He wants to calculate his final result.
+// He has 6 subjects in his 6th semester.
+// to calculate the final result he need to calculate the average of each subject and 
+// calculate the average of all the subjects.
+// if the average marks is greater than 70 then he will be promoted to next semester.
+// if the average marks is less than 70 then he will be failed in the semester.
+// write a program to calculate the final result of Nitish.
+// Input: 6 subjects marks  
+
+public class assignment {
+}
---
+++ day1/sum.java
@@ -0,0 +1,21 @@
+public class sum {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        if (a > b){
+            if (a > c){
+                System.out.println("a is the largest number");
+            } else {
+                System.out.println("c is the largest number");
+            }
+        } else {
+            if (b > c){
+                System.out.println("b is the largest number");
+            } else {
+                System.out.println("c is the largest number");
+            }
+        }
+    }
+}
